a3b8e201d362ae5fa9a797ffd7f11fbfbb76bede
DatIntrovert03 <60486964+DatIntrovert03@users.noreply.github.com>
2022-01-18 13:47:54 +0200

    I have added the timer and corrected the index problem

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace headfirstcsharp_project
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
+	using System.Windows.Threading;
+
 	public partial class MainWindow : Window
 	{
+		DispatcherTimer timer = new DispatcherTimer();
+		int tenthsofsecondselapsed;
+		int matchesfound;
+
 		public MainWindow()
 		{
 			InitializeComponent();
-
+			timer.Interval = TimeSpan.FromSeconds(.1);
+			timer.Tick += Timer_Tick;
 			SetupGame();
 
+		}
+
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			tenthsofsecondselapsed++;
+			timetextblock.Text = (tenthsofsecondselapsed / 10F).ToString("0.0s");
+			if(matchesfound==8)
+			{
+				timer.Stop();
+				timetextblock.Text = timetextblock.Text + "-Play again?";
+			}
 		}
 
 		private void SetupGame()
@@ -44,12 +51,53 @@ namespace headfirstcsharp_project
 			Random random = new Random();
 			foreach (TextBlock textBlock in maingrid.Children.OfType<TextBlock>())
 			{
-				int index = random.Next(animalemoji.Count);//
-				string nextemoji = animalemoji[index];
-				textBlock.Text = nextemoji;
-				animalemoji.RemoveAt(index);
+				if (textBlock.Name != "timetextblock")
+				{
+					textBlock.Visibility = Visibility.Visible;
+					int index = random.Next(animalemoji.Count);//
+					string nextemoji = animalemoji[index];
+					textBlock.Text = nextemoji;
+					animalemoji.RemoveAt(index);
+				}
+				
 			}
+			timer.Start();
+			tenthsofsecondselapsed = 0;
+			matchesfound = 0;
 
+		}
+		TextBlock lasttextblock;
+		bool findingMatch = false;
+
+		private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			TextBlock textblock = sender as TextBlock;
+
+			if (findingMatch==false)
+			{
+				textblock.Visibility = Visibility.Hidden;
+				lasttextblock = textblock;
+				findingMatch = true;
+			}
+			else if(textblock.Text==lasttextblock.Text)
+			{
+				matchesfound++;
+				textblock.Visibility = Visibility.Hidden;
+				findingMatch = false;
+			}
+			else
+			{
+				lasttextblock.Visibility = Visibility.Visible;
+				findingMatch = false;
+			}
+		}
+
+		private void timetextblock_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if (matchesfound==8)
+			{
+				SetupGame();
+			}
 		}
 	}
 }
